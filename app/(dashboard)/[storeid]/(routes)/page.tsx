375c2d9c3d7a1d3c44ab0f2fd8a8475da62a37d1
faizkhoirurrizqi <faizkhoirurrizqinoor@gmail.com>
2023-12-17 00:57:02 +0700

    done

--- app/(dashboard)/[storeid]/(routes)/page.tsx
+++ app/(dashboard)/[storeid]/(routes)/page.tsx
@@ -1,14 +1,14 @@
-// import { CreditCard, DollarSign, Package } from "lucide-react";
+import { CreditCard, DollarSign, Package } from "lucide-react";
 
-// import { Separator } from "@/components/ui/separator";
-// import { Overview } from "@/components/overview";
-// import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-// import { Heading } from "@/components/ui/heading";
-// import { getTotalRevenue } from "@/actions/get-total-revenue";
-// import { getSalesCount } from "@/actions/get-sales-count";
-// import { getGraphRevenue } from "@/actions/get-graph-revenue";
-// import { getStockCount } from "@/actions/get-stock-count";
-// import { formatter } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
+import { Overview } from "@/components/overview";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Heading } from "@/components/ui/heading";
+import { getTotalRevenue } from "@/actions/get-total-revenue";
+import { getSalesCount } from "@/actions/get-sales-count";
+import { getGraphRevenue } from "@/actions/get-graph-revenue";
+import { getStockCount } from "@/actions/get-stock-count";
+import { formatter } from "@/lib/utils";
 
 interface DashboardPageProps {
   params: {
@@ -17,15 +17,15 @@ interface DashboardPageProps {
 }
 
 const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
-  //   const totalRevenue = await getTotalRevenue(params.storeId);
-  //   const graphRevenue = await getGraphRevenue(params.storeId);
-  //   const salesCount = await getSalesCount(params.storeId);
-  //   const stockCount = await getStockCount(params.storeId);
+  const totalRevenue = await getTotalRevenue(params.storeId);
+  const graphRevenue = await getGraphRevenue(params.storeId);
+  const salesCount = await getSalesCount(params.storeId);
+  const stockCount = await getStockCount(params.storeId);
 
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        {/* <Heading title="Dashboard" description="Overview of your store" />
+        <Heading title="Dashboard" description="Overview of your store" />
         <Separator />
         <div className="grid gap-4 grid-cols-3">
           <Card>
@@ -69,7 +69,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
           <CardContent className="pl-2">
             <Overview data={graphRevenue} />
           </CardContent>
-        </Card> */}
+        </Card>
       </div>
     </div>
   );
